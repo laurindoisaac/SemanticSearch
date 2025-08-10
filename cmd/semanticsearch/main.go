66cd23f11fbd9a16013d6ecf88212c009bb8563c
Lauri <116688165+laurindoisaac@users.noreply.github.com>
2025-08-10 10:21:51 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/semanticsearch/main.go
@@ -0,0 +1,23 @@
+// cmd/semanticsearch/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "semanticsearch/internal/semanticsearch"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := semanticsearch.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
